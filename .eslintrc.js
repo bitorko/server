55c8e71d1ff4694d6ab3935b7afe3714e7634e99
Sayem <afm.sayem@gmail.com>
2016-10-10 20:48:49 +0600

    eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "parser": "babel-eslint",
+  "extends": "airbnb-base",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "generator-star-spacing": 0
+  }
+};
