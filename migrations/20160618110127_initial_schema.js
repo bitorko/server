c7ad91af8f65bd9cb90575e033daf38941f864c6
Sayem <afm.sayem@gmail.com>
2016-10-12 18:28:28 +0600

    Add checks for data

--- migrations/20160618110127_initial_schema.js
+++ migrations/20160618110127_initial_schema.js
@@ -2,24 +2,25 @@ function up(knex) {
   return knex.schema
     .createTable('institution', (table) => {
       table.increments('id').primary();
-      table.string('name');
+      table.string('name').notNullable();
+      table.string('country_code').defaultTo('bd');
     })
     .createTable('person', (table) => {
       table.increments('id').primary();
       table.integer('institution_id').unsigned().references('id').inTable('institution');
-      table.string('name');
+      table.string('name').notNullable();
     })
     .createTable('motion_type', (table) => {
       table.increments('id').primary();
-      table.string('name');
+      table.string('name').notNullable();
     })
     .createTable('tournament', (table) => {
       table.increments('id').primary();
-      table.integer('host_id').unsigned().references('id').inTable('institution');
-      table.string('name');
+      table.integer('host_id').unsigned().references('id').inTable('institution').notNullable();
+      table.string('name').notNullable();
       table.text('description');
       table.string('poster');
-      table.integer('rounds');
+      table.integer('rounds').notNullable();
       table.date('start_date');
       table.date('end_date');
     })
@@ -27,10 +28,10 @@ function up(knex) {
       table.increments('id').primary();
       table.integer('tournament_id').unsigned().references('id').inTable('tournament');
       table.integer('motion_type_id').unsigned().references('id').inTable('motion_type');
-      table.integer('number');
-      table.integer('teams');
-      table.integer('adjudicators');
-      table.string('motion');
+      table.integer('number').notNullable();
+      table.integer('team_count');
+      table.integer('adj_count');
+      table.string('motion').notNullable();
       table.text('context');
     })
     .createTable('room', (table) => {
@@ -40,18 +41,17 @@ function up(knex) {
     })
     .createTable('team', (table) => {
       table.increments('id').primary();
-      table.string('name');
+      table.string('name').notNullable();
       table.integer('tournament_id').unsigned().references('id').inTable('tournament');
       table.integer('institution_id').unsigned().references('id').inTable('institution');
       table.specificType('position', 'integer[]');
-      table.string('name');
-      table.boolean('swing');
-      table.boolean('active');
+      table.boolean('swing').defaultTo(false);
+      table.boolean('active').defaultTo(true);
     })
     .createTable('user', (table) => {
       table.increments('id').primary();
       table.integer('person_id').unsigned().references('id').inTable('person');
-      table.string('email');
+      table.string('email').notNullable().unique();
       table.string('facebook');
       table.string('hash');
       table.string('name');
@@ -60,18 +60,19 @@ function up(knex) {
     .createTable('tournament_adjudicator', (table) => {
       table.increments('id').primary();
       table.integer('tournament_id').unsigned().references('id').inTable('tournament');
-      table.integer('person_id').unsigned().references('id').inTable('person');
-      table.string('name');
+      table.integer('adjudicator_id').unsigned().references('id').inTable('person');
+      table.boolean('can_chair');
+      table.boolean('active').defaultTo(true);
     })
     .createTable('team_member', (table) => {
       table.increments('id').primary();
       table.integer('team_id').unsigned().references('id').inTable('team');
-      table.integer('person_id').unsigned().references('id').inTable('person');
+      table.integer('member_id').unsigned().references('id').inTable('person');
     })
     .createTable('room_adjudicator', (table) => {
       table.increments('id').primary();
       table.integer('room_id').unsigned().references('id').inTable('room');
-      table.integer('person_id').unsigned().references('id').inTable('person');
+      table.integer('adjudicator_id').unsigned().references('id').inTable('person');
       table.enu('role', ['chair', 'panelist', 'trainee']);
       table.integer('points');
     })
@@ -87,11 +88,12 @@ function up(knex) {
       table.integer('points');
       table.enu('role', ['og', 'oo', 'co', 'cg']);
     })
-    .createTable('room_team_member', (table) => {
+    .createTable('room_team_debater', (table) => {
       table.increments('id').primary();
       table.integer('room_team_id').unsigned().references('id').inTable('room_team');
-      table.integer('person_id').unsigned().references('id').inTable('person');
+      table.integer('debater_id').unsigned().references('id').inTable('person');
       table.integer('score');
+      table.integer('speech_order');
     });
 }
 
@@ -102,6 +104,7 @@ function down(knex) {
     .dropTableIfExists('room_team_member')
     .dropTableIfExists('room_adjudicator')
     .dropTableIfExists('team_member')
+    .dropTableIfExists('tournament_adjudicator')
     .dropTableIfExists('user')
     .dropTableIfExists('team')
     .dropTableIfExists('room')
---
+++ migrations/20161011141617_constraints.js
@@ -0,0 +1,19 @@
+function up(knex, Promise) {
+  return Promise.all([
+    knex.raw('ALTER TABLE tournament ADD CONSTRAINT round_limit CHECK (rounds > 0 AND rounds < 9)'),
+    knex.raw('ALTER TABLE round ADD CONSTRAINT round_limit CHECK (number > 0 AND number < 9)'),
+    knex.raw('ALTER TABLE room_team ADD CONSTRAINT point_limit CHECK (points >= 0 AND points <= 3)'),
+    knex.raw('ALTER TABLE room_team_member ADD CONSTRAINT score_limit CHECK (score >= 0 AND score <= 100)'),
+  ]);
+}
+
+function down(knex, Promise) {
+  return Promise.all([
+    knex.raw('ALTER TABLE tournament DROP CONSTRAINT round_limit'),
+    knex.raw('ALTER TABLE round DROP CONSTRAINT round_limit'),
+    knex.raw('ALTER TABLE room_team DROP CONSTRAINT point_limit'),
+    knex.raw('ALTER TABLE room_team_member DROP CONSTRAINT score_limit'),
+  ]);
+}
+
+module.exports = { up, down };
