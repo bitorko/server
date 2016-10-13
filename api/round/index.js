290d296a2b4f85dbb1f4f53a3480657d052a2362
Sayem <afm.sayem@gmail.com>
2016-10-13 19:19:19 +0600

    CRUD endpoints for rounds

--- api/round/index.js
+++ api/round/index.js
@@ -3,16 +3,19 @@ const Round = require('./round.model');
 const BaseController = require('../base/base.controller');
 const processQuery = require('../../components/middlewares/process-query');
 
-const controller = new BaseController(Round);
+const controller = new BaseController(Round, 'round_id');
 
-const router = new express.Router();
+const router = new express.Router({ mergeParams: true });
+const roomRouter = require('../room');
+
+router.use('/:round_id/rooms', roomRouter);
 
 router.get('/', processQuery, controller.index.bind(controller));
-router.get('/:id', controller.show.bind(controller));
+router.get('/:round_id', controller.show.bind(controller));
 router.post('/', controller.create.bind(controller));
-router.put('/:id', controller.update.bind(controller));
-router.patch('/:id', controller.update.bind(controller));
-router.delete('/:id', controller.destroy.bind(controller));
+router.put('/:round_id', controller.update.bind(controller));
+router.patch('/:round_id', controller.update.bind(controller));
+router.delete('/:round_id', controller.destroy.bind(controller));
 
 module.exports = router;
 
--- api/round/round.model.js
+++ api/round/round.model.js
@@ -1,5 +1,6 @@
 const Model = require('objection').Model;
 const schema = require('./round.schema.json');
+const path = require('path');
 
 class Round extends Model {
   static get tableName() {
@@ -8,6 +9,19 @@ class Round extends Model {
 
   static get jsonSchema() {
     return schema;
+  }
+
+  static get relationshipMappings() {
+    return {
+      rooms: {
+        relation: Model.HasManyRelation,
+        modelClass: path.normalize(`${__dirname}/../room/room.model`),
+        join: {
+          from: 'round.id',
+          to: 'room.round_id',
+        },
+      },
+    };
   }
 }
 
