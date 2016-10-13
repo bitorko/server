6a9781610081acf1f65b03b5b543384d190b53fd
Sayem <afm.sayem@gmail.com>
2016-10-13 19:19:37 +0600

    CRUD endpoints for team and team member

--- api/team/team.model.js
+++ api/team/team.model.js
@@ -1,5 +1,6 @@
 const Model = require('objection').Model;
 const schema = require('./team.schema.json');
+const path = require('path');
 
 class Team extends Model {
   static get tableName() {
@@ -8,6 +9,24 @@ class Team extends Model {
 
   static get jsonSchema() {
     return schema;
+  }
+
+  static get relationshipMappings() {
+    return {
+      members: {
+        relation: Model.ManyToManyRelation,
+        modelClass: path.normalize(`${__dirname}/../person/person.model`),
+        join: {
+          from: 'team.id',
+          through: {
+            from: 'team_member.team_id',
+            to: 'team_member.member_id',
+            extra: ['role', 'points'],
+          },
+          to: 'person.id',
+        },
+      },
+    };
   }
 }
 
---
+++ api/team/team_member.model.js
@@ -0,0 +1,23 @@
+const Model = require('../base/base.model');
+const path = require('path');
+
+class RoomTeam extends Model {
+  static get tableName() {
+    return 'room_team';
+  }
+
+  static get relationshipMappings() {
+    return {
+      debaters: {
+        relation: Model.HasManyRelation,
+        modelClass: path.normalize(`${__dirname}/../room/room_team_debater.model`),
+        join: {
+          from: 'room_team.id',
+          to: 'room_team_debater.room_team_id',
+        },
+      },
+    };
+  }
+}
+
+module.exports = RoomTeam;
